09f11cd88d466243add6386a995244e2f389a17c
Ryan Pederson <pedersor@uci.edu>
2020-07-04 19:33:13 -0700

    svd_decomposition to base_mps
    does full SVD at the moment.. will need to change to truncate correctly

--- tensornetwork/matrixproductstates/base_mps.py
+++ tensornetwork/matrixproductstates/base_mps.py
@@ -98,6 +98,11 @@ class BaseMPS:
     ########################################################################
     ##########       define functions for jitted operations       ##########
     ########################################################################
+    def svd_decomposition(tensor):
+      return self.backend.svd_decomposition(tensor, 2)
+
+    self.svd_decomposition = self.backend.jit(svd_decomposition)
+
     def qr_decomposition(tensor):
       return self.backend.qr_decomposition(tensor, 2)
 
